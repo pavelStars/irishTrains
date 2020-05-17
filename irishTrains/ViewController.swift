b523cb30e2edc61c026af65fa661f9e763008654
Pavel Terziyski <pavelterziyskiblg@abv.bg>
2020-05-17 07:36:44 +0300

    Initial Commit

---
+++ irishTrains/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  irishTrains
+//
+//  Created by Pavel Terziyski on 17.05.20.
+//  Copyright © 2020 Pavel Terziyski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
